02874b0471d7298a8e107d53620af0ba129218de
Carol Willing <carolcode@willingconsulting.com>
2014-11-10 07:01:59 -0800

    Complete section 3

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
 class Entry(models.Model):
     title = models.CharField(max_length=500)
     author = models.ForeignKey('auth.User')
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+    	return reverse('entry_detail', kwargs={'pk':self.pk})
+
+    class Meta:
+        verbose_name_plural = "entries"
 
--- myblog/urls.py
+++ myblog/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myblog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+import blog.urls
+from . import views
 
+urlpatterns = [
+    url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^', include(blog.urls)),
     url(r'^admin/', include(admin.site.urls)),
-)
+]
